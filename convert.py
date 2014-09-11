090d672354e568f9e67f1b6f8f3f1d1734c8fc2f
dstar <g@d-str.cc>
2014-09-11 18:09:46 +0000

    minor changes

--- convert.py
+++ convert.py
@@ -42,17 +42,17 @@ def main():
     stix_header = STIXHeader()
     stix_header.title = "Indicators"
     stix_header.add_package_intent ("Indicators - Watchlist")
-
+    # XXX add Information_Source and Handling
     contain_pkg.stix_header = stix_header
 
 
     # create kill chain with three options (pre, post, unknown), relate as needed
-    pre = KillChainPhaseReference(phase_id="u5:pre", name="Pre-infection indicator", ordinality=1,kill_chain_id="u5:KillChain-7808F4B3-61A1-429C-9E05-1D7B9D18A895")
-    post = KillChainPhaseReference(phase_id="u5:post", name="Post-infection indicator", ordinality=2,kill_chain_id="u5:KillChain-7808F4B3-61A1-429C-9E05-1D7B9D18A895")
-    unk = KillChainPhaseReference(phase_id="u5:unknown", name="Unknown",kill_chain_id="u5:KillChain-7808F4B3-61A1-429C-9E05-1D7B9D18A895")
-    chain = KillChain(id_="u5:KillChain-7808F4B3-61A1-429C-9E05-1D7B9D18A895")
+    pre = KillChainPhaseReference(phase_id="stix:KillChainPhase-1a3c67f7-5623-4621-8d67-74963d1c5fee", name="Pre-infection indicator", ordinality=1,kill_chain_id="stix:KillChain-3fbfebf2-25a7-47b9-ad8b-3f65e56e402d")
+    post = KillChainPhaseReference(phase_id="stix:KillChainPhase-d5459305-1a27-4f50-9875-23793d75e4fe", name="Post-infection indicator", ordinality=2,kill_chain_id="stix:KillChain-3fbfebf2-25a7-47b9-ad8b-3f65e56e402d")
+    chain = KillChain(id_="stix:KillChain-3fbfebf2-25a7-47b9-ad8b-3f65e56e402d", name="Degenerate Cyber Kill Chain"  )
+    chain.definer = "U5"
 
-    chain.kill_chain_phases = [pre, post, unk]
+    chain.kill_chain_phases = [pre, post]
     contain_pkg.ttps.kill_chains.append(chain)
 
     # read input data
@@ -77,13 +77,12 @@ def main():
             ind.kill_chain_phases.append(pre.phase_id)
         elif 'Post' in row['InfectionType']:
             ind.kill_chain_phases.append(post.phase_id)
-        else:
-            ind.kill_chain_phases.append(unk.phase_id)
  
 
         # XXX we omit indicatortype from output since it's implied in cybox type 
         ind_type = row['IndicatorType']
         if 'IP' in ind_type:
+            ind.add_indicator_type ("IP Watchlist")
             ind_obj = SocketAddress()
             ind_obj.ip_address = row['Indicator']
             ind_obj.ip_address.condition= "Equals"
@@ -95,17 +94,22 @@ def main():
 
 
         elif 'Domain' in ind_type:
+            ind.add_indicator_type ("Malicious E-mail")
             ind_obj = DomainName()
             ind_obj.value = row['Indicator']
             ind_obj.value.condition= "Equals"
 
         elif 'Email' in ind_type:
+            
+            ind.add_indicator_type ("Domain Watchlist")
             ind_obj = EmailMessage()
+            
             ind_obj.subject = row['Indicator']
             ind_obj.subject.condition= "Equals"
             # XXX unknown where real data keeps sender name
 
-        elif 'UserAgent' in ind_type:
+        elif 'User Agent' in ind_type:
+            ind.add_indicator_type ("C2")
             # XXX this method can be used to encode other HTTP headers as well
             fields = HTTPRequestHeaderFields()
             fields.user_agent = row['Indicator']
@@ -120,6 +124,8 @@ def main():
             ind_obj.http_request_response = [request]
             
         elif 'URI' in ind_type:
+            ind.add_indicator_type ("URL Watchlist")
+    
             request = HTTPClientRequest()
             request.http_request_line = HTTPRequestLine()
             request.http_request_line.http_method = row['Indicator'].split()[0]
@@ -132,6 +138,7 @@ def main():
 
 
         elif 'File' in ind_type:
+            ind.add_indicator_type ("File Hash Watchlist")
             ind_obj = File()
             ind_obj.file_name = row['Indicator']
             ind_obj.file_name.condition = "Equals"
@@ -139,10 +146,13 @@ def main():
             # XXX assumes that hash digests are stored in this field in real data
             digest.simple_hash_value = row['indValue']
             digest.simple_hash_value.condition = "Equals"
+            digest.type_.condition = "Equals"
 
             ind_obj.add_hash(digest)
 
         elif 'Registry' in ind_type:
+            ind.add_indicator_type ("Host Characteristics")
+            
             ind_obj = WinRegistryKey()
             keys = RegistryValues()
             key = RegistryValue()
@@ -154,6 +164,8 @@ def main():
             ind_obj.values = keys
 
         elif 'Mutex' in ind_type:
+            
+            ind.add_indicator_type ("Host Characteristics")
             ind_obj = Mutex()
             ind_obj.name = row['Indicator']
             ind_obj.name.condition= "Equals"
